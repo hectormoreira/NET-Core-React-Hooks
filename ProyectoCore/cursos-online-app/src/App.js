2b545dd894e23d22ef6c30bdb9d1acaf1ec92543
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2021-01-08 13:11:29 -0500

    material ui

--- ProyectoCore/cursos-online-app/src/App.js
+++ ProyectoCore/cursos-online-app/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import MuiThemeprovider from "@material-ui/core/styles/MuiThemeProvider";
+import theme from "./theme/theme";
+import { Button, TextField } from "@material-ui/core";
 
 function App() {
   return(
-    <h1>Proyecto vacío</h1>
+    <MuiThemeprovider theme={theme}>
+      <h1>Proyecto vacío</h1>
+      <TextField variant="outlined" />
+      <Button variant="contained" color="primary"> Mi boton material </Button>
+    </MuiThemeprovider>
   )
 }
 
