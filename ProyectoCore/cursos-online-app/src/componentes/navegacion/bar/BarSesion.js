b3f16d1a26704a2fcf530f79f42771f16d9c35c3
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2021-01-23 17:29:37 -0500

    navbar responsive

--- ProyectoCore/cursos-online-app/src/componentes/navegacion/bar/BarSesion.js
+++ ProyectoCore/cursos-online-app/src/componentes/navegacion/bar/BarSesion.js
@@ -1,15 +1,60 @@
-import { IconButton, Toolbar, Typography } from '@material-ui/core';
-import React from 'react';
+import {
+  IconButton,
+  Toolbar,
+  Typography,
+  makeStyles,
+  Button,
+  Avatar,
+} from "@material-ui/core";
+import React from "react";
+import fotoUsuarioTemp from "../../../logo.svg";
+
+const useStyle = makeStyles((theme) => ({
+  seccionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+  seccionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  avatarSize: {
+    width: 40,
+    height: 40,
+  },
+}));
 
 const BarSesion = () => {
-    return (
-        <Toolbar>
-            <IconButton color="inherit">
-                <i className="material-icons">menu</i>
-            </IconButton>
-            <Typography variant="h6">Cursos Online</Typography>
-        </Toolbar>
-    );
+  const classes = useStyle();
+
+  return (
+    <Toolbar>
+      <IconButton color="inherit">
+        <i className="material-icons">menu</i>
+      </IconButton>
+      <Typography variant="h6">Cursos Online</Typography>
+      <div className={classes.grow}></div>
+
+      <div className={classes.seccionDesktop}>
+        <Button color="inherit">Salir</Button>
+        <Button color="inherit">{"Nombre de usuario"}</Button>
+        <Avatar src={fotoUsuarioTemp}></Avatar>
+      </div>
+
+      <div className={classes.seccionMobile}>
+          <IconButton color="inherit">
+              <i className="material-icons">more_vert</i>
+          </IconButton>
+      </div>
+    </Toolbar>
+  );
 };
 
 export default BarSesion;
