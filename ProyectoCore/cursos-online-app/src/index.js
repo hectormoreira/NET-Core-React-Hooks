91b1eb6533f282b78fcfbb1babd986634a7ca41e
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2021-01-25 17:35:18 -0500

    implementar provider

--- ProyectoCore/cursos-online-app/src/index.js
+++ ProyectoCore/cursos-online-app/src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initialState } from "./contexto/initialState";
+import { StateProvider } from "./contexto/store";
+import { mainReducer } from "./contexto/reducers";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={mainReducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
