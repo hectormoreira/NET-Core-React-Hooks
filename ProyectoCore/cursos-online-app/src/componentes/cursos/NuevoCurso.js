07023910cc4e7f157fbcdb5e3053a020112ceac6
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2021-01-31 17:02:01 -0500

    validacion curso e imagen

--- ProyectoCore/cursos-online-app/src/componentes/cursos/NuevoCurso.js
+++ ProyectoCore/cursos-online-app/src/componentes/cursos/NuevoCurso.js
@@ -16,9 +16,10 @@ import ImageUploader from "react-images-upload";
 import { v4 as uuidv4 } from "uuid";
 import { obtenerDataImagen } from "../../actions/imagenAction";
 import { guardarCurso } from "../../actions/CursoAction";
-import { parse } from "date-fns";
+import { useStateValue } from "../../contexto/store";
 
 const NuevoCurso = () => {
+  const [{ sesionUsuario }, distpach] = useStateValue();
   const [fechaSeleccionada, setFechaSeleccionada] = useState(new Date());
   const [imagenCurso, setImagenCurso] = useState(null);
   const [curso, setCurso] = useState({
@@ -27,6 +28,17 @@ const NuevoCurso = () => {
     precio: 0.0,
     promocion: 0.0,
   });
+
+  const resetForm = () => {
+    setFechaSeleccionada(new Date());
+    setImagenCurso(null);
+    setCurso({
+      titulo: "",
+      descripcion: "",
+      precio: 0.0,
+      promocion: 0.0,
+    });
+  };
 
   const ingresarValores = (e) => {
     const { name, value } = e.target;
@@ -38,35 +50,63 @@ const NuevoCurso = () => {
 
   const subirFoto = (imagenes) => {
     const foto = imagenes[0];
-
     obtenerDataImagen(foto).then((respuesta) => {
-        setImagenCurso(respuesta);
+      setImagenCurso(respuesta);
     });
   };
 
-  const guardarCursoBoton = e =>{
-      e.preventDefault();
-      const cursoId = uuidv4();
-      const objetoCurso = {
-        titulo: curso.titulo,
-        descripcion: curso.descripcion,
-        promocion: parseFloat(curso.promocion || 0.0),
-        precio: parseFloat(curso.precio || 0.0),
-        fechaPublicacion: fechaSeleccionada,
-        cursoId : cursoId
-      };
+  const guardarCursoBoton = (e) => {
+    e.preventDefault();
+    const cursoId = uuidv4();
+    const objetoCurso = {
+      titulo: curso.titulo,
+      descripcion: curso.descripcion,
+      promocion: parseFloat(curso.promocion || 0.0),
+      precio: parseFloat(curso.precio || 0.0),
+      fechaPublicacion: fechaSeleccionada,
+      cursoId: cursoId,
+    };
 
-      const objetoImagen = {
+    let objetoImagen = null;
+    if (imagenCurso) {
+      objetoImagen = {
         nombre: imagenCurso.nombre,
         data: imagenCurso.data,
         extension: imagenCurso.extension,
-        objetoReferencia: cursoId
+        objetoReferencia: cursoId,
+      };
+    }
+
+    guardarCurso(objetoCurso, objetoImagen).then((respuestas) => {
+      const responseCurso = respuestas[0];
+      const responseImagen = respuestas[1];
+
+      let mensaje = "";
+      if (responseCurso.status === 200) {
+        mensaje += "Se guardó exitosamente el curso";
+        resetForm();
+      } else {
+        mensaje += "Errores: " + Object.keys(responseCurso.data.errors);
       }
 
-      guardarCurso(objetoCurso, objetoImagen).then(respuestas =>{
-          console.log('respuesta array', respuestas)
-      })
-  }
+      if (responseImagen) {
+        if (responseCurso.status === 200) {
+          mensaje += ", se guardó la imagen correctamente";
+        } else {
+          mensaje +=
+            ", errores en imagen: " + Object.keys(responseCurso.data.errors);
+        }
+      }
+
+      distpach({
+        type: "OPEN_SNACKBAR",
+        openMensaje: {
+          open: true,
+          mensaje: mensaje,
+        },
+      });
+    });
+  };
 
   const fotoKey = uuidv4();
 
