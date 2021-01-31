9ff675f3ab3c3be870892ae0253e2f8e8cc1b39c
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2021-01-31 11:02:01 -0500

    upload para imagen

--- ProyectoCore/cursos-online-app/src/componentes/seguridad/PerfilUsuario.js
+++ ProyectoCore/cursos-online-app/src/componentes/seguridad/PerfilUsuario.js
@@ -5,145 +5,174 @@ import {
   Typography,
   Button,
   Grid,
+  Avatar,
 } from "@material-ui/core";
 import style from "../tool/Style";
-import { actualizarUsuario, obtenerUsuarioActual } from "../../actions/usuarioAction";
+import {
+  actualizarUsuario,
+  obtenerUsuarioActual,
+} from "../../actions/usuarioAction";
 import { useStateValue } from "../../contexto/store";
+import reactFoto from "../../logo.svg";
+import {v4 as uuidv4} from "uuid";
+import ImageUploader from "react-images-upload";
 
 const PerfilUsuario = () => {
   const [{ sesionUsuario }, dispatch] = useStateValue();
-
 
   const [usuario, setUsuario] = useState({
     nombreCompleto: "",
     password: "",
     confirmarPassword: "",
     email: "",
-    userName: ""
-  })
+    userName: "",
+    foto: ""
+  });
 
-  const IngresarValores = e =>{
-    const {name, value} = e.target;
-    setUsuario(anterior => ({
+  const IngresarValores = (e) => {
+    const { name, value } = e.target;
+    setUsuario((anterior) => ({
       ...anterior,
-      [name]: value
+      [name]: value,
     }));
-  }
+  };
 
-  useEffect(() =>{
-    obtenerUsuarioActual(dispatch).then(response =>{
+  useEffect(() => {
+    obtenerUsuarioActual(dispatch).then((response) => {
       console.log("data del usuario actual ", response);
       setUsuario(response.data);
     });
-  }, [])
+  }, []);
 
-  const guardarUsuario = e =>{
+  const guardarUsuario = (e) => {
     e.preventDefault();
-    actualizarUsuario(usuario).then(response =>{
+    actualizarUsuario(usuario).then((response) => {
       if (response.status === 200) {
         dispatch({
           type: "OPEN_SNACKBAR",
           openMensaje: {
             open: true,
-            mensaje: "Se guardaron exitosamente los cambios en Perfil de usuario"
-          }
-        })
+            mensaje:
+              "Se guardaron exitosamente los cambios en Perfil de usuario",
+          },
+        });
         window.localStorage.setItem("token_seguridad", response.data.token);
-      }else{
+      } else {
         dispatch({
           type: "OPEN_SNACKBAR",
           openMensaje: {
             open: true,
-            mensaje: "Errores al intentar guardar en: " + Object.keys(response.data.errors)
-          }
-        })
+            mensaje:
+              "Errores al intentar guardar en: " +
+              Object.keys(response.data.errors),
+          },
+        });
       }
-    })
+    });
+  };
+
+  const fotoKey = uuidv4();
+  const subirFoto = imagenes =>{
+    
   }
 
   return (
     <Container component="main" maxWidth="md" justify="center">
       <div style={style.paper}>
+        <Avatar style={style.avatar} src={usuario.foto || reactFoto}></Avatar>
         <Typography component="h1" variant="h5">
           Perfil de usuario
         </Typography>
+
+        <form style={style.form}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={12}>
+              <TextField
+                name="nombreCompleto"
+                variant="outlined"
+                fullWidth
+                label="Ingrese nombre y apellidos"
+                onChange={IngresarValores}
+                value={usuario.nombreCompleto}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <TextField
+                name="userName"
+                variant="outlined"
+                fullWidth
+                label="Ingrese su username"
+                onChange={IngresarValores}
+                value={usuario.userName}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <TextField
+                name="email"
+                variant="outlined"
+                fullWidth
+                label="Ingrese su email"
+                onChange={IngresarValores}
+                value={usuario.email}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <TextField
+                name="password"
+                variant="outlined"
+                type="password"
+                fullWidth
+                label="Ingrese su password"
+                onChange={IngresarValores}
+                value={usuario.password}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <TextField
+                name="confirmarPassword"
+                variant="outlined"
+                type="password"
+                fullWidth
+                label="Confirmar su password"
+                onChange={IngresarValores}
+                value={usuario.confirmarPassword}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={12}>
+              <ImageUploader
+                withIcon={false}
+                key={fotoKey}
+                singleImage={true}
+                buttonText="Seleccione imagen de perfil"
+                onChange={subirFoto}
+                imgExtension={[".jpg",".gif",".png",".jpeg"]}
+                maxFileSize={5242880}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid container justify="center">
+            <Grid item xs={12} md={6}>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                size="large"
+                style={style.submit}
+                onClick={guardarUsuario}
+              >
+                Guardar datos
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
       </div>
-      <form style={style.form}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={12}>
-            <TextField
-              name="nombreCompleto"
-              variant="outlined"
-              fullWidth
-              label="Ingrese nombre y apellidos"
-              onChange={IngresarValores}
-              value={usuario.nombreCompleto}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6}>
-            <TextField
-              name="userName"
-              variant="outlined"
-              fullWidth
-              label="Ingrese su username"
-              onChange={IngresarValores}
-              value={usuario.userName}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6}>
-            <TextField
-              name="email"
-              variant="outlined"
-              fullWidth
-              label="Ingrese su email"
-              onChange={IngresarValores}
-              value={usuario.email}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6}>
-            <TextField
-              name="password"
-              variant="outlined"
-              type="password"
-              fullWidth
-              label="Ingrese su password"
-              onChange={IngresarValores}
-              value={usuario.password}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6}>
-            <TextField
-              name="confirmarPassword"
-              variant="outlined"
-              type="password"
-              fullWidth
-              label="Confirmar su password"
-              onChange={IngresarValores}
-              value={usuario.confirmarPassword}
-            />
-          </Grid>
-        </Grid>
-
-        <Grid container justify="center">
-          <Grid item xs={12} md={6}>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary" 
-              size="large"
-              style={style.submit}
-              onClick={guardarUsuario}
-            >
-              Guardar datos
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
     </Container>
   );
 };
